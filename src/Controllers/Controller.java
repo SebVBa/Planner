c53f47601cd2c2b77a3604a625365385c7f9c041
jprodriguerzb <jprodriguerzb@gmail.com>
2023-08-06 15:47:48 -0600

    Interface Controller
    Se crea la interface controller que define el comportamiento de los controladores del proyecto

---
+++ src/Controllers/Controller.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+
+/**
+ *
+ * @author jprod
+ */
+public interface Controller<Type> {
+    public void insert(Type obj);
+    public void update(Type obj);
+    public void delete(Type obj);
+    public void read(Object id);
+    public void readAll();
+}
